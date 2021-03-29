b289e6c8956857e39ad3c40d77423740e6772118
Patryk Mróz <patrykmroz619@wp.pl>
2021-03-29 17:05:40 +0200

    Add projects' content animations.

--- src/ts/features/portfolio/contentSlider/index.ts
+++ src/ts/features/portfolio/contentSlider/index.ts
@@ -1,6 +1,52 @@
+import { gsap } from 'gsap';
 import { setActiveProjectsNavigationItem } from '../navigation';
 
 const projects = [...document.querySelectorAll('.portfolio__project')];
+
+const getProjectsElements = (project: Element) => {
+    const name = project.querySelector('.project__name');
+    const description = project.querySelector('.project__descriptionWrapper');
+    const technologiesHeading = project.querySelector(
+        '.project__technologiesHeading'
+    );
+    const technologies = project.querySelectorAll('.project__technology');
+    const links = project.querySelectorAll('.project__link');
+
+    return { name, description, technologiesHeading, technologies, links };
+};
+
+const revealAnimation = (project: Element) => {
+    const {
+        name,
+        description,
+        technologiesHeading,
+        technologies,
+        links,
+    } = getProjectsElements(project);
+
+    const timeline = gsap.timeline();
+
+    timeline.to(name, { opacity: 1, y: 0 });
+    timeline.to(description, { opacity: 1, y: 0 }, '-=0.25');
+    timeline.to(technologiesHeading, { opacity: 1, y: 0 }, '-=0.25');
+    technologies.forEach((technology) => {
+        timeline.to(technology, { opacity: 1, y: 0 }, '-=0.4');
+    });
+    links.forEach((link) => {
+        timeline.to(link, { opacity: 1, y: 0 }, '-=0.25');
+    });
+};
+
+const hideAnimation = (project: Element) => {
+    const projectElements = getProjectsElements(project);
+
+    const timeline = gsap.timeline();
+
+    timeline.to(Object.values(projectElements), { opacity: 0 });
+    timeline.set(Object.values(projectElements), {
+        y: 25,
+    });
+};
 
 export const setActiveProject = (index: number) => {
     setActiveProjectsNavigationItem(index);
@@ -8,8 +54,10 @@ export const setActiveProject = (index: number) => {
     for (let i = 0; i < projects.length; i++) {
         if (i === index) {
             projects[i].classList.add('active');
+            revealAnimation(projects[i]);
         } else {
             projects[i].classList.remove('active');
+            hideAnimation(projects[i]);
         }
     }
 };
