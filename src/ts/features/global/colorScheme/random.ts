d9a8d70ef848d5f89dea659db604c61e7e864c50
Patryk Mróz <patrykmroz619@wp.pl>
2021-04-11 19:16:19 +0200

    Add new random colors.

--- src/ts/features/global/colorScheme/random.ts
+++ src/ts/features/global/colorScheme/random.ts
@@ -5,12 +5,7 @@ type ColorVariants = {
 
 const red: ColorVariants = {
     dark: '#FC5130',
-    light: '#FC5130',
-};
-
-const purple: ColorVariants = {
-    dark: '#7e50d3',
-    light: '#7e50d3',
+    light: '#dc300f',
 };
 
 const blue: ColorVariants = {
@@ -18,12 +13,22 @@ const blue: ColorVariants = {
     light: '#1891bb',
 };
 
+const darkBlue: ColorVariants = {
+    dark: '#3e4ec4',
+    light: '#3e4ec4',
+};
+
 const green: ColorVariants = {
     dark: '#17d14f',
     light: '#19a242',
 };
 
-const colors: ColorVariants[] = [red, purple, blue, green];
+const orange: ColorVariants = {
+    dark: '#d17e17',
+    light: '#f18f14',
+};
+
+const colors: ColorVariants[] = [red, blue, green, orange, darkBlue];
 
 export const getRandomColor = () => {
     const randomIndex = Math.floor(Math.random() * colors.length);
